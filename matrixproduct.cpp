c0481793427e42a13bf5475312f0d353aebcba6e
Camilo Santana <camilocsm@gmail.com>
2022-03-23 23:41:59 +0000

    line multiplication working

--- matrixproduct.cpp
+++ matrixproduct.cpp
@@ -60,7 +60,49 @@ void OnMult(int m_ar, int m_br){
 
 // add code here for line x line matriz multiplication
 void OnMultLine(int m_ar, int m_br){
+	SYSTEMTIME time1, time2;
+	
+	char st[100];
+	int i, j, k;
+	double *pha, *phb, *phc;
 
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)calloc((m_ar * m_ar), sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+    time1 = clock();
+
+	for(i=0; i<m_ar; i++){
+		for( k=0; k<m_ar; k++){
+			for( j=0; j<m_br; j++){
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+		}
+	}
+
+    time2 = clock();
+
+	sprintf(st, "Time: %3.3f seconds\n", (double)(time2 - time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix to verify correctness
+	cout << "Result matrix: " << endl;
+	for(j=0; j<min(10,m_br); j++)
+		cout << phc[j] << " ";
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
 }
 
 // add code here for block x block matriz multiplication
